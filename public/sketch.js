1db8ce4b4fa80e3d500d00e957a605cecfff7def
Omar V <omar.vazquezac@gmail.com>
2016-04-22 00:22:18 -0500

    sockets and many clients at the time done, ergo, tutorial done!

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,30 @@
+var socket;
+
 function setup() {
 	createCanvas(500, 500);
+	background(51);
+	socket = io.connect("http://localhost:3000");
+	socket.on('mouse', function(data) {
+		noStroke();
+		fill(0, 0, 255);
+		ellipse(data.x, data.y, 6, 6);
+	});
+}
+
+function mouseDragged() {
+	console.log(mouseX + ', ' + mouseY);
+
+	var data = {
+		x: mouseX,
+		y: mouseY
+	}
+
+	socket.emit('mouse', data);
+	
+	noStroke();
+	fill(255);
+	ellipse(mouseX, mouseY, 6, 6);
 }
 
 function draw() {
-	background(51);
-	ellipse(mouseX, mouseY, 6, 6);
 }
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-var express = require('express');
+var express = require('express'),
+	socket = require('socket.io');
 
-var app = express();
-var server = app.listen(3000);
+////////// Express //////////
+var app = express(),
+	server = app.listen(3000);
 
 app.use(express.static('public'));
+
+////////// Socket.io /////////
+var io = socket(server);
+
+io.sockets.on('connection', function(socket) {
+	socket.on('mouse', function(data) {
+		socket.broadcast.emit('mouse', data);
+		console.log(data);
+	});
+});
+
+
 
 console.log("Socket server running");
